4ee056a074e066cd12daa5c08015e6dc83f9e2f3
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-11-16 16:48:28 +0700

    add Search function

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -78,5 +78,16 @@ namespace DoublyLinkedList
             previous.next = newnode;
         }
 
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            for(previous = current = START; current != null &&
+                rollNo != current.rollNumber; previous = current, 
+                current = current.next) 
+            { }
+            //The above for loop traverses the list. If the specified node
+            //is found the the function returns true, otherwise false
+            return(current != null);
+        }
+
     }
 }
