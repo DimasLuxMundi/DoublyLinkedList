377ef900603c45f50d3846a789036de694237314
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-11-16 16:24:47 +0700

    add Node class

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace DoublyLinkedList
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
-        {
-        }
+        /*Node class represents the node of doubly linked list
+         *It consists of the information part and links to
+         *its succeeding and preceeding nodes
+         *in terms of next adn previous nodes.*/
+
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the succeeding node*/
+        public Node prev;/*points to the precceeding node*/
     }
 }
